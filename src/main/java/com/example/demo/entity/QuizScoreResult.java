824f514384e77a8fad55762a908f9e46a2a0cd7b
Lilitaleqyan <lilitaleqyan01@mail.ru>
2026-04-17 19:54:41 +0400

    Fixed quiz score results and auth controller

--- src/main/java/com/example/demo/entity/QuizScoreResult.java
+++ src/main/java/com/example/demo/entity/QuizScoreResult.java
@@ -2,6 +2,7 @@ package com.example.demo.entity;
 
 import com.example.demo.entity.quizConstructor.Quiz;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -14,10 +15,11 @@ public class QuizScoreResult {
     private Long id;
 
     @ManyToOne
-    @JsonBackReference
+    @JsonBackReference("player_result")
     private Player player;
 
     @ManyToOne
+    @JsonBackReference
     private Quiz quiz;
 
     private Integer bestScore;
